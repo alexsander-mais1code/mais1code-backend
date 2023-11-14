ed2c5bf39bc3e2b1e9adc4c1507d369e97e40dce
Alexsander <alexsanderdeoliveira@gmail.com>
2023-11-14 13:49:40 -0300

    feature: ajuste config

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 const config = Object.freeze({
     database: {
-        host: 'dpg-cjv7i815mpss73e81620-a.oregon-postgres.render.com',
-        user: 'mais1code_postgresql_user',
-        password: 's8ene4z7w2Sw5Kw35mjtCb0ErZSKKL0u',
-        database: 'mais1code_postgresql',
+        host: '',
+        user: '',
+        password: '',
+        database: '',
         port: 5432
     }
 });
